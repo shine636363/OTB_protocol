22026a9108632b3cc67fd740c1fd2a9c8ba29bcb
linboqiao <linboqiao@gmail.com>
2016-05-13 19:40:14 +0800

    batch process update

--- run_task.m
+++ run_task.m
@@ -5,8 +5,8 @@ cd ../OTB_protocol
 
 main_running
 
-system('git rm -rf ./tmp');
 
+system('git pull');
 system('git add  ./results');
 system('git commit -m "resutlts-20160513"');
 system('git push');
